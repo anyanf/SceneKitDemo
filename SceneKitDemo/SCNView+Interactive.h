023b0392571814ec83dfdfb168a2443aad4345a0
ankang <ankang-ds@gomeplus.com>
2017-08-31 18:15:32 +0800

    抽类

---
+++ SceneKitDemo/SCNView+Interactive.h
@@ -0,0 +1,13 @@
+//
+//  SCNView+Interactive.h
+//  SceneKitDemo
+//
+//  Created by kang an on 2017/8/31.
+//  Copyright © 2017年 张贝贝. All rights reserved.
+//
+
+#import <SceneKit/SceneKit.h>
+
+@interface SCNView (Interactive)
+
+@end
---
+++ SceneKitDemo/SCNView+Interactive.m
@@ -0,0 +1,13 @@
+//
+//  SCNView+Interactive.m
+//  SceneKitDemo
+//
+//  Created by kang an on 2017/8/31.
+//  Copyright © 2017年 张贝贝. All rights reserved.
+//
+
+#import "SCNView+Interactive.h"
+
+@implementation SCNView (Interactive)
+
+@end
